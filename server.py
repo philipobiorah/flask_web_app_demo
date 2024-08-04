7c1ea25058662bc26278134c2db8350f980c6712
PHILIP OBIORAH <philip.obiorah@outlook.com>
2024-08-04 20:19:26 +0100

    added about route and blog route

--- server.py
+++ server.py
@@ -8,6 +8,19 @@ def home():
     return 'Hello World'
 
 
+@app.route('/about')
+def about():
+    return "This is about page"
+
+
+@app.route('/blog')
+def blog():
+    return "This is my blog"
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000, debug=True)
+
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run()
